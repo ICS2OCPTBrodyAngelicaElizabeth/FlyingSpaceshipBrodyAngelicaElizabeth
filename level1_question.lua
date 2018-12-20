fca4b9771ae99d1e900ba0b67936b72963ba6634
AngelicaL714 <angelica.lutkiewicz@ocsbstudent.ca>
2018-12-20 13:55:41 -0500

    working timer in level 1

--- level1_question.lua
+++ level1_question.lua
@@ -130,7 +130,7 @@ local function TouchListenerAnswer(touch)
     
     if (touch.phase == "ended") then
 
-        questionCorrect = questionCorrect + 1
+        questionCorrect1FS = questionCorrect1FS + 1
         BackToLevel1( )
     end 
 end
@@ -311,7 +311,7 @@ function scene:create( event )
     wrongText3 = display.newText("", X2, Y1, Arial, 75)
     wrongText3.anchorX = 0
     -----------------------------------------------------------------------------------------
-    clockText = display.newText("", display.contentWidth *10/12, display.contentHeight *11/12, nil, 50)
+    clockText = display.newText("", display.contentWidth *3/12, display.contentHeight *1/12, nil, 50)
     clockText:setTextColor(1)
 
     -----------------------------------------------------------------------------------------
