6dcc6b3d4bb73e268f6abc4d93cd2498a362213a
BrodyLawson <43419130+BrodyLawson@users.noreply.github.com>
2019-01-07 14:17:12 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -18,10 +18,3 @@ local composer = require( "composer" )
 
 -- Go to the intro screen
 composer.gotoScene( "splash_screen" )
-
-		if (randomNumber1 < randomNumber2) then
-	 		temp = randomNumber1
-	 		randomNumber1 = randomNumber2
-	 		randomNumber2 = temp
-	 		correctAnswer = randomNumber1 - randomNumber2
-	 	end
