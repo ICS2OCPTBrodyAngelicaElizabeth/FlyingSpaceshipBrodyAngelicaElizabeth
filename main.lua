fdae9d33560414c4ded805be1c59b2905c2e5ce0
BrodyLawson <43419130+BrodyLawson@users.noreply.github.com>
2019-01-10 13:53:26 -0500

    Fixed Level Select

--- main.lua
+++ main.lua
@@ -14,7 +14,8 @@ display.setStatusBar(display.HiddenStatusBar)
 -- Use composer library
 local composer = require( "composer" )
 
+
 -----------------------------------------------------------------------------------------
 
 -- Go to the intro screen
-composer.gotoScene( "splash_screen2" )
+composer.gotoScene( "level_select" )
