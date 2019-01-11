3199d4997fbec3a96ff963fc800ad4dded768f9d
AngelicaL714 <angelica.lutkiewicz@ocsbstudent.ca>
2019-01-11 14:06:59 -0500

    comments

--- main.lua
+++ main.lua
@@ -14,8 +14,7 @@ display.setStatusBar(display.HiddenStatusBar)
 -- Use composer library
 local composer = require( "composer" )
 
-
 -----------------------------------------------------------------------------------------
 
 -- Go to the intro screen
-composer.gotoScene( "splash_screen2" )
+composer.gotoScene( "level1_screen" )
