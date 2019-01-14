cc121d5673f3c2f07bb669c614348799b975265a
ElizabethAcheng <43419208+ElizabethAcheng@users.noreply.github.com>
2019-01-14 13:39:02 -0500

    update

--- main.lua
+++ main.lua
@@ -14,7 +14,8 @@ display.setStatusBar(display.HiddenStatusBar)
 -- Use composer library
 local composer = require( "composer" )
 
+
 -----------------------------------------------------------------------------------------
 
 -- Go to the intro screen
-composer.gotoScene( "level1_screen" )
+composer.gotoScene( "splash_screen2" )
